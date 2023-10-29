4c02bf2314b97840ca9728e7423f0352e1228b49
Giang Vu Long <longgiang2015bn@gmail.com>
2023-10-29 23:45:07 +0700

    change number of epoch

--- utils/train_base.py
+++ utils/train_base.py
@@ -29,7 +29,7 @@ def train(model, dataset, log_file_name="", log_folder="log", clamp_value=-1, fr
     batch_size = 64
     workers = 5
     lr = 0.01
-    num_epoch = 100
+    num_epoch = 200
     # num_epoch = 1
     weight_decay = 1e-4
     momentum = 0.9
