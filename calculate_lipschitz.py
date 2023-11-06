7c79ecd5f2a598c3f6c291d10c02ae4534bd8d75
Giang Vu Long <longgiang2015bn@gmail.com>
2023-11-06 21:43:05 +0700

    fix

--- calculate_lipschitz.py
+++ calculate_lipschitz.py
@@ -29,6 +29,8 @@ def cal_lipschitz(args):
             model_output.append(output.detach().cpu())
 
     model_output = torch.concatenate(model_output)
+    print(model_output.shape)
+    print(model_output.sum(dim=1))
     _, num_class = model_output.shape
     # model_output = torch.nn.functional.one_hot(torch.argmax(model_output, dim=1), num_class).float()
 
